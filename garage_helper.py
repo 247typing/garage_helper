c1fef48546240a260e85b6eb26e3ad7ddf5772a7
247typing <lloyd.rem700.308@gmail.com>
2020-04-14 20:28:29 -0600

    set up the base program test and got it working. Still need to update some things to automate the process of buttons then lots of formatting work

--- garage_helper.py
+++ garage_helper.py
@@ -1,6 +1,32 @@
 import tkinter as tk
 import sys, inspect
 
+#returns a list of dicts for each class in this module with attribute is_tool = True
+#dicts contain "name": the exact name of the class, "obj": the object itself, "nice_name": used for labeling buttons etc.
+def find_all_tools():
+    #get a list of all classes in the module
+    classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+
+    #sort out the name of each class and its attribute of is_tool
+    #returns
+    tool_classes = [] #??? change to dict with class, name, and nice name ???
+    for cls in classes:
+        temp_obj = eval(cls[0])
+
+        #string name of class
+        print(cls[0])
+        #class attribute is_tool
+        print(temp_obj.is_tool)
+        if temp_obj.is_tool:
+            info = {
+                "name" : cls[0],
+                "obj" : temp_obj,
+                "nice_name" : temp_obj.nice_name
+            }
+            tool_classes.append(info)
+
+    print(tool_classes)
+    return tool_classes
 
 #The Control code was derived from this tutorial
 #https://pythonprogramming.net/tkinter-depth-tutorial-making-actual-program/
@@ -16,7 +42,16 @@ class Control(tk.Tk):
 
         frame = Welcome(container, self)
         self.frames[Welcome] = frame
-        frame.pack()
+        #frame.pack()
+        frame.grid(row=0, column=0, sticky="nsew")
+
+        #test to add all tools
+        for i in range(len(selections)):
+            temp_obj = selections[i]["obj"]
+            frame = temp_obj(container, self)
+            self.frames[temp_obj] = frame
+            #frame.pack()
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(Welcome)
 
@@ -31,26 +66,42 @@ class Selection_Menu(tk.Frame):
         label = tk.Label(self, text="Selection_Menu Page")
         label.pack()
 
+        #TODO set the buttons to auto allocate as many as tools
+                                                                        #TODO change Test_tool to automatic from selections
+        button = tk.Button(self, text=selections[0]["nice_name"], command= lambda: controller.show_frame(Test_Tool))
+        button.pack()
+
+        button1 = tk.Button(self, text=selections[1]["nice_name"], command= lambda: controller.show_frame(Test_Two))
+        button1.pack()
 #class Welcome(tk.Frame):
 class Welcome(Selection_Menu, tk.Frame):
-    is_tool = True
+    is_tool = False
     def __init__(self, parent, controller):
         super().__init__(parent,controller)
         label = tk.Label(self, text="Welcome Page")
         label.pack()
 
+class Test_Tool(Selection_Menu, tk.Frame):
+    is_tool = True
+    nice_name = "Test Tool"
+    def __init__(self, parent, controller):
+        super().__init__(parent,controller)
+        label = tk.Label(self, text="Test Tool Page")
+        label.pack()
+
+class Test_Two(Selection_Menu, tk.Frame):
+    is_tool = True
+    nice_name = "Test Tool 2"
+    def __init__(self, parent, controller):
+        super().__init__(parent,controller)
+        label = tk.Label(self, text="Test 2 Tool Page")
+        label.pack()
+
+
+selections = find_all_tools()
+print(selections)
+print(type(selections))
+
+#launch the app
 app = Control()
 app.mainloop()
-
-#get a list of all classes in the module
-classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-
-#sort out the name of each class and its attribute of is_tool
-#used for getting all tools automaticaly
-for cls in classes:
-    print(cls[0])
-    print(eval(cls[0]).is_tool)
-
-
-
-#print(inspect.getmembers(Control))
