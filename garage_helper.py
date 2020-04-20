438581c7d962640432f477a2930b57964d1a562c
unknown <lloyd.rem700.308@gmail.com>
2020-04-19 18:40:26 -0600

    split the frames for the tools and selection buttons. These still move between welcome and tool. Started working of the drill tap calculator

--- garage_helper.py
+++ garage_helper.py
@@ -33,6 +33,27 @@ def find_all_tools():
     print("max tool width: " + str(max_tool_width))
     return max_tool_width, max_tool_height, tool_classes
 
+#This funciton takes a single entry and performs basic error checking and division
+#accepted inputs are integers, floats, and fractions with '/' character
+#returns float if acceptable other wise returns "ERROR"
+def check_entry(entry):
+    #check if entry is empty
+    if entry:
+        #try to convert to float directly
+        try:
+            solution = float(entry)
+            return solution
+        except ValueError:
+            #if that doesn't work, check for single '/' indicating division
+            fraction = entry.split('/')
+            if len(fraction) == 2:
+                solution = float(fraction[0]) / float(fraction[1])
+                return solution
+        solution = "ERROR"
+    else:
+        print("empty")
+    return solution
+
 #The Control code was derived from this tutorial
 #https://pythonprogramming.net/tkinter-depth-tutorial-making-actual-program/
 class Control(tk.Tk):
@@ -64,51 +85,97 @@ class Control(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-#######################################
-#should make this its own frame so the buttons are not tied to the rows/cols of tools
-###################################################################
+#This is a special class that contains the top frame that contains buttons to select tools
 class Selection_Menu(tk.Frame):
     is_tool = False
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
 
+        selection_frame = tk.Frame(self)
+        selection_frame.grid(row=0, column=0)
 
         #create a button for each "tool" class in this module
         buttons = []
         for i in range(len(selections)):
             #the use of j = i lambda is to differentiate which lambda to use so the correct screen is called
-            new_button = tk.Button(self, text=selections[i]["nice_name"], command = lambda j = i: controller.show_frame(selections[j]["obj"]))
-            new_button.grid(row=1, column=i)
+            new_button = tk.Button(selection_frame,
+                                text=selections[i]["nice_name"],
+                                font = BUTTON_FONT,
+                                command = lambda j = i: controller.show_frame(selections[j]["obj"]),
+                                anchor="w")
 
+            #used to be self
+            new_button.grid(row=0, column=i, padx=10, pady = 10)
+
+#special class that doesn't have a button in the selection menu
+#this is the welcome screen and cannot be accessed after selecting a tool
 class Welcome(Selection_Menu, tk.Frame):
     is_tool = False
     def __init__(self, parent, controller):
         super().__init__(parent,controller)
-        label = tk.Label(self, text="Welcome Page")
-        label.grid(row=2, column=0)
+        tool_frame = tk.Frame(self)
+        tool_frame.grid(row = 1, column = 0)
+        label = tk.Label(tool_frame, text="Welcome Page", font=DEFAULT_TOOL_FONT)
+        label.grid(row=0, column=0)
+        label = tk.Label(tool_frame,
+                        text="Select the tool you would like to use from the buttons above",
+                        font=DEFAULT_TOOL_FONT,
+                        wraplength=max_tool_width,
+                        justify="left")
 
-class Test_Tool(Selection_Menu, tk.Frame):
+        label.grid(row=1, column=0)
+
+class Drill_Tap_Chart(Selection_Menu, tk.Frame):
     is_tool = True
-    nice_name = "Test Tool"
+    nice_name = "Drill Tap Chart"
     tool_width = 400
     tool_height = 200
     def __init__(self, parent, controller):
         super().__init__(parent,controller)
-        label = tk.Label(self, text="Test Tool Page")
-        label.grid(row=2, column=0)
+        tool_frame = tk.Frame(self)
+        tool_frame.grid(row = 1, column = 0)
+
+        label = tk.Label(tool_frame, text=self.nice_name, font=DEFAULT_TOOL_FONT)
+        label.grid(row=0, column=0)
+
+        self.tap_dia_entry = tk.Entry(tool_frame, font=DEFAULT_TOOL_FONT)
+        self.tap_dia_entry.grid(row=1, column=0)
+
+        self.tap_TPI_entry = tk.Entry(tool_frame, font=DEFAULT_TOOL_FONT)
+        self.tap_TPI_entry.grid(row=1, column=1)
+
+        calc_button = tk.Button(tool_frame, text="Calculate", font=DEFAULT_TOOL_FONT, command=self.get_entries)
+        calc_button.grid(row=2, column=0)
+
+
+    def get_entries(self):
+        print(self.tap_dia_entry.get())
+        print(self.tap_TPI_entry.get())
+
+        tap_dia = check_entry(self.tap_dia_entry.get())
+        if tap_dia == "ERROR":
+            pass #change to popup message indicating error
+        
+        #clear the text in the boxes
+        self.tap_dia_entry.delete(0, 'end')
+        self.tap_TPI_entry.delete(0, 'end')
+
 
 class Test_Two(Selection_Menu, tk.Frame):
     is_tool = True
     nice_name = "Test Tool 2"
-    tool_width = 500
+    tool_width = 800
     tool_height = 500
     def __init__(self, parent, controller):
         super().__init__(parent,controller)
-        label = tk.Label(self, text="Test 2 Tool Page")
+        tool_frame = tk.Frame(self)
+        tool_frame.grid(row = 1, column = 0)
+        label = tk.Label(tool_frame, text="Test 2 Tool Page", font=DEFAULT_TOOL_FONT)
         label.grid(row=2, column=0)
 
-
-
+#TODO needs to be moved to a config file #####################
+BUTTON_FONT = ("arial", 18)
+DEFAULT_TOOL_FONT = ("arial", 16)
 
 
 
